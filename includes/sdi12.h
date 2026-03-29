7347c2fe3e81785caf8d279b6b6fb9d71ed6f698
Keijo Länsikunnas <keijo.lansikunnas@metropolia.fi>
2026-03-30 00:30:16 +0300

    Improved SDI-12 receive. Added flush before a command is sent

--- includes/sdi12.h
+++ includes/sdi12.h
@@ -6,5 +6,6 @@
 int sdi12_init(void);
 int sdi12_cmd(const char *cmd, bool send_break);
 int sdi12_wait_for(char *buffer, int size, const char *expect);
+int sdi12_flush(void);
 
 #endif
--- src/device/sdi12_scan.c
+++ src/device/sdi12_scan.c
@@ -3,6 +3,9 @@
 #include <zephyr/drivers/uart.h>
 #include <zephyr/sys/printk.h>
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(teros, CONFIG_MODEM_MODULES_LOG_LEVEL);
+
 #include <stdio.h>
 #include <time.h>
 
@@ -12,26 +15,34 @@
 #include "data_types.h"
 #include "data_queue.h"
 
-
-void sdi12_scan_thread(void *arg0, void *arg1,  void *arg2)
+void sdi12_scan_thread(void *arg0, void *arg1, void *arg2)
 {
     char response[128];
-    struct sensor_data data = { .type = TYPE_TEROS12, .timestamp = 0, .id = "Unknown SDI-12 device" };
+    struct sensor_data data = {.type = TYPE_TEROS12, .timestamp = 0, .id = "Unknown SDI-12 device"};
 
     sdi12_init();
 
-    sdi12_cmd("0I!", true);
-    if (sdi12_wait_for(response, sizeof(response), "0I!") > 0)
+    char *str = NULL;
+
+    do
     {
-        // record sensor id
-        char *str = strstr(response, "METER   TER12");
-        if(str) {
-            strncpy(data.id, str + 8, sizeof(data.id)); // copy string starting from "TER"
-            data.id[sizeof(data.id) -1] = 0; // ensure termination
-            data.id[strcspn(data.id, " ")] = '-'; // replace space with dash
-            data.id[strcspn(data.id, "\n\r")] = 0; // remove CR/LF
+        sdi12_cmd("0I!", true);
+        if (sdi12_wait_for(response, sizeof(response), "0I!") > 0)
+        {
+            // record sensor id
+            str = strstr(response, "METER   TER12");
+            if (str)
+            {
+                strncpy(data.id, str + 8, sizeof(data.id)); // copy string starting from "TER"
+                data.id[sizeof(data.id) - 1] = 0;           // ensure termination
+                data.id[strcspn(data.id, " ")] = '-';       // replace space with dash
+                data.id[strcspn(data.id, "\n\r")] = 0;      // remove CR/LF
+            }
         }
-    }
+        else {
+            k_msleep(2000);
+        }
+    } while (str == NULL);
 
     while (true)
     {
@@ -40,23 +51,30 @@ void sdi12_scan_thread(void *arg0, void *arg1,  void *arg2)
         {
             // extract values
             char *rsp = strstr(response, "0R0!0");
-            if(!rsp) continue; // should not happens since wait_for found a match
-
+            if (!rsp)
+            {
+                LOG_ERR("No match found");
+                continue; // should not happens since wait_for found a match
+            }
             int rv = sscanf(rsp + 5, "%f%f%f", &data.teros.vwc, &data.teros.temp, &data.teros.ec);
-            if(rv == 3) {
+            if (rv == 3)
+            {
                 // with single sensor the id is set during init, jsut set timestamp
                 data.timestamp = time(NULL);
                 // teros 12
-                if(data_put(&data) < 0) {
-                    // error
+                if (data_put(&data) < 0)
+                {
+                    LOG_ERR("Failed to queue data");
                 }
-                
             }
-#if 0
-            else if(rv == 2) {
-                // teros 11 - not implemented yet
+            else
+            {
+                LOG_ERR("Data parse failed");
             }
-#endif
+        }
+        else
+        {
+            LOG_INF("Response timeout");
         }
 
         k_msleep(MEASURE_CYCLE_SLEEP);
