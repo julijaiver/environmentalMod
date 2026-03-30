6a481899bc3953420173ff2e7eb3f16b6a6ccbe4
Keijo Länsikunnas <keijo.lansikunnas@metropolia.fi>
2026-03-30 16:18:23 +0300

    Replaced ad-hoc base64 encode

--- src/network/jwt.c
+++ src/network/jwt.c
@@ -1,6 +1,5 @@
 // Includes
 #include "jwt.h"
-#include "base64.h"
 #include "private.h"
 #include "realtime.h"
 
@@ -21,6 +20,8 @@
 // RTOS
 #include <zephyr/kernel.h>
 
+#include <zephyr/sys/base64.h>
+#include <zephyr/data/jwt.h>
 
 
 void jwt_init(jwt_t *jwt){
@@ -35,7 +36,7 @@ void jwt_init(jwt_t *jwt){
     jwt->payload.scope = JWT_SCOPE;
     jwt->payload.iss = JWT_ISSUER;
     jwt->payload.sub = JWT_SUBJECT;
-    jwt->payload.iat = get_current_time();// - (3600 * 3); // Adjust to UCT
+    jwt->payload.iat = get_current_time();// clock runs in UTC
     jwt->payload.exp = jwt->payload.iat + 3600;
 }
 
@@ -59,26 +60,45 @@ char *jwt_construct_payload(struct payload *payload){
     return result;
 }
 
-char *jwt_build(unsigned char *header, struct payload payload){
-    
-    unsigned char signature[MBEDTLS_PK_SIGNATURE_MAX_SIZE];
-    size_t signature_len, header_len, payload_len;
 
-    char *constructed_header_payload = (char*)k_malloc(1024);
+char *jwt_build(unsigned char *header, struct payload payload){
+    size_t output_len = 0;
+
     unsigned char *jwt = (char*)k_malloc(2048);
-    unsigned char hash[32];
+
+    struct jwt_builder builder = {};
+	builder.base = jwt;
+	builder.buf = jwt;
+	builder.len = 2048;
+	builder.overflowed = false;
+	builder.pending = 0;
+
+
+    if(base64_encode(builder.buf, builder.len, &output_len, header, strlen(header))) {
+        printk("Header encode failed\n");
+        k_free(jwt);
+        return NULL;
+    }
+    builder.buf[output_len] = '.';
+    builder.buf += output_len + 1;
+    builder.len -= output_len + 1;
 
     char *constructed_payload = jwt_construct_payload(&payload);
-    header_len = strlen((char*)header);
-    char *encoded_header = base64_encrypt(header, header_len, 0);
-    payload_len = strlen(constructed_payload);
-    char *encoded_payload = base64_encrypt(constructed_payload, payload_len, 0);
-    char *encoded_signature;
-    k_free(constructed_payload);
+    if(base64_encode(builder.buf, builder.len, &output_len, constructed_payload, strlen(constructed_payload))) {
+        printk("Payload encode failed\n");
+        k_free(constructed_payload);
+        k_free(jwt);
+        return NULL;
+    }
+    builder.buf += output_len;
+    builder.len -= output_len;
     
-   
-    snprintf(constructed_header_payload, JWT_SIGNATURE_SIZE, "%s.%s", encoded_header, encoded_payload);
+    k_free(constructed_payload);
 
+    // sign jwt
+    unsigned char signature[MBEDTLS_PK_SIGNATURE_MAX_SIZE];
+    unsigned char hash[32];
+    size_t signature_len = 0;
     mbedtls_pk_context pk;
     mbedtls_pk_init(&pk);
 
@@ -105,21 +125,25 @@ char *jwt_build(unsigned char *header, struct payload payload){
     }
 
     
-    mbedtls_sha256(constructed_header_payload, strlen((char*)constructed_header_payload), hash, 0);
-    k_free(constructed_header_payload);
-    
+    mbedtls_sha256(builder.base, builder.buf - builder.base, hash, 0);
+
     ret = mbedtls_pk_sign(&pk, MBEDTLS_MD_SHA256, hash, 0, signature, MBEDTLS_PK_SIGNATURE_MAX_SIZE, &signature_len, mbedtls_ctr_drbg_random, &ctr_drbg);
     if (ret != 0) {
         printk("ERROR: mbedtls_pk_sign FAILED with code %d (0x%x)\n", ret, -ret);
         goto cleanup;
     }
     
-    encoded_signature = base64_encrypt(signature, signature_len, 0);
-    snprintf(jwt, JWT_SIGNATURE_SIZE, "%s.%s.%s", encoded_header, encoded_payload, encoded_signature);
+    builder.buf[0] = '.';
+    builder.buf += 1;
+    builder.len -= 1;
 
-    k_free(encoded_header);
-    k_free(encoded_payload);
-    k_free(encoded_signature);
+    if(base64_encode(builder.buf, builder.len, &output_len, signature, signature_len)) {
+        printk("ERROR: failed to encode signature!\n");
+        goto cleanup;
+    }
+    builder.buf[output_len] = 0;
+    builder.buf += output_len;
+    builder.len -= output_len;
 
     mbedtls_pk_free(&pk);
     mbedtls_entropy_free(&entropy);
@@ -133,9 +157,50 @@ cleanup:
     mbedtls_entropy_free(&entropy);
     mbedtls_ctr_drbg_free(&ctr_drbg);
     mbedtls_psa_crypto_free();
-    k_free(encoded_header);
-    k_free(encoded_payload);
-    k_free(constructed_header_payload);
+    k_free(jwt);
     return NULL;    
-    
+
+#if 0
+    unsigned char *der = k_malloc(strlen(private_key_pem));
+    unsigned char *s1 = (unsigned char *) strstr((const char *) private_key_pem, "-----BEGIN");
+    if(s1 == NULL) {
+        k_free(der);
+        k_free(jwt);
+        return NULL;
+    }
+    s1+=10; // skip -----BEGIN
+    while(*s1 && *s1 != '-') ++s1; // skip until dashes
+    while(*s1 =='-') ++s1;
+    while(*s1 =='\r') ++s1;
+    while(*s1 =='\n') ++s1;
+    unsigned char *s2 = (unsigned char *) strstr((const char *) s1, "-----END");
+    if (s2 == NULL) {
+        k_free(jwt);
+        return NULL;
+    }
+    int keylen = s2 - s1;
+
+    printk("%.10s\n", s1);
+    printk("%.10s\n",s2);
+    int res =base64_decode(der, strlen(private_key_pem), &output_len, s1, keylen);
+    if(res) {
+        printk("PEM decode failed %d %d, %d\n", res, (int)output_len, keylen);
+        k_free(der);
+        k_free(jwt);
+        return NULL;
+    }
+
+    // this fails - reports that kay is not valid - some field not found 
+    res = jwt_sign(&builder, der, output_len);
+    if(res) {
+        printk("Message sign failed %d\n", res);
+        k_free(der);
+        k_free(jwt);
+        return NULL;
+    }
+    k_free(der);
+    *builder.buf=0;
+
+    return jwt;   
+#endif
 }
