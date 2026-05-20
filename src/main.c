96240ab064da9298b6a728241ffbcabfe2e10aba
Keijo Länsikunnas <keijo.lansikunnas@metropolia.fi>
2026-05-20 13:44:59 +0300

    Added ADC and enabled shell help

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/rtc.h>
+#include <zephyr/drivers/adc.h>
 #include <zephyr/logging/log.h>
 
 // Standard libraries
@@ -21,6 +22,25 @@
 #include "nv_params.h"
 
 LOG_MODULE_REGISTER(main);
+
+/* ADC node from the devicetree. */
+#define ADC_NODE DT_ALIAS(adc0)
+
+/* Auxiliary macro to obtain channel vref, if available. */
+#define CHANNEL_VREF(node_id) DT_PROP_OR(node_id, zephyr_vref_mv, 0)
+
+/* Data of ADC device specified in devicetree. */
+static const struct device *adc = DEVICE_DT_GET(ADC_NODE);
+
+/* Data array of ADC channels for the specified ADC. */
+static const struct adc_channel_cfg channel_cfgs[] = {
+	DT_FOREACH_CHILD_SEP(ADC_NODE, ADC_CHANNEL_CFG_DT, (,))};
+
+/* Data array of ADC channel voltage references. */
+static uint32_t vrefs_mv[] = {DT_FOREACH_CHILD_SEP(ADC_NODE, CHANNEL_VREF, (,))};
+
+/* Get the number of channels defined on the DTS. */
+#define CHANNEL_COUNT ARRAY_SIZE(channel_cfgs)
 
 #if CONFIG_SDI12
 K_THREAD_STACK_DEFINE(sdi12_stack_area, SDI12_STACKSIZE);
@@ -48,6 +68,13 @@ void boot_continue(void)
 int main(void)
 {
 	struct tm rtc_time;
+	int rc;
+#ifdef CONFIG_SEQUENCE_32BITS_REGISTERS
+	uint32_t channel_reading[CONFIG_SEQUENCE_SAMPLES][CHANNEL_COUNT];
+#else
+	uint16_t channel_reading[CONFIG_SEQUENCE_SAMPLES][CHANNEL_COUNT];
+#endif
+
 
 	nv_params_init();
 
@@ -78,6 +105,40 @@ int main(void)
 #endif
 	boot_continue();
 
+	/* Options for the sequence sampling. */
+	const struct adc_sequence_options options = {
+		.extra_samplings = CONFIG_SEQUENCE_SAMPLES - 1,
+		.interval_us = 0,
+	};
+
+	/* Configure the sampling sequence to be made. */
+	struct adc_sequence sequence = {
+		.buffer = channel_reading,
+		/* buffer size in bytes, not number of samples */
+		.buffer_size = sizeof(channel_reading),
+		.resolution = CONFIG_SEQUENCE_RESOLUTION,
+		.options = &options,
+	};
+
+	if (!device_is_ready(adc)) {
+		printf("ADC controller device %s not ready\n", adc->name);
+		return 0;
+	}
+
+	/* Configure channels individually prior to sampling. */
+	for (size_t i = 0U; i < CHANNEL_COUNT; i++) {
+		sequence.channels |= BIT(channel_cfgs[i].channel_id);
+		rc = adc_channel_setup(adc, &channel_cfgs[i]);
+		if (rc < 0) {
+			printf("Could not setup channel #%d (%d)\n", i, rc);
+			return 0;
+		}
+		if ((vrefs_mv[i] == 0) && (channel_cfgs[i].reference == ADC_REF_INTERNAL)) {
+			vrefs_mv[i] = adc_ref_internal(adc);
+		}
+	}
+
+
 	uint16_t err = setup(&rtc_time); // this mostly 4G-modem related setup
 
 	while (err != ERR_NONE)
@@ -100,6 +161,32 @@ int main(void)
 				LOG_INF("Start sampling");
 				print_current_time();
 				TAKE_SAMPLE_NOW(); 
+				rc = adc_read(adc, &sequence);
+				if (rc >= 0) {
+					for (size_t channel_index = 0U; channel_index < CHANNEL_COUNT; channel_index++) {
+						int32_t val_mv;	
+
+						// just read the first sample even if we have more than one
+						// todo: cleanup setup
+						val_mv = channel_reading[0][channel_index];
+
+						rc = adc_raw_to_millivolts(vrefs_mv[channel_index],
+										channel_cfgs[channel_index].gain,
+										CONFIG_SEQUENCE_RESOLUTION, &val_mv);
+						/* conversion to mV may not be supported, skip if not */
+						if ((rc < 0) || vrefs_mv[channel_index] == 0) {
+							LOG_ERR("value in mV not available");
+						} else {
+							LOG_INF("Battery voltage: %d", (int)(val_mv * 5.6)); // multiplier 5.6 based on voltage divider
+							// todo: report battery voltage
+						}
+					
+					}
+				}
+				else {
+					LOG_ERR("Could not read ADC (%d)", rc);
+				}
+
 				k_msleep(MEASURE_CYCLE_MINUTES * 60000 - 5000); // go to sleep and wake up 5s before next deadline
 			}
 			else {
